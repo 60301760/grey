b5c44bc258c3cea347e6bde087f712e102908e64
60301760 <60301760@udst.edu.qa>
2025-03-28 09:28:13 +0300

    h

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from public/
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the root folder
+app.use(express.static(__dirname));
 
+// Serve index.html for root route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Grey Coffee running at http://localhost:${PORT}`);
 });
